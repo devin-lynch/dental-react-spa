6776bd36b770d9830177e900cc815e499489be90
devin-lynch <devin.l.lynch@gmail.com>
2022-09-30 12:32:57 -0500

    Add nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
 import Home from "./components/pages/Home";
 import Services from "./components/pages/Services"
 import Contact from "./components/pages/Contact"
 
 export default function App() {
   return (
-    <div>
-      <Home />
-    </div>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Go to Home Page</Link>{' | '}
+          <Link to="/services">See Our Services</Link>{' | '}
+          <Link to="/contact">Contact Us!</Link>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='services/*' element={<Services />} />
+          <Route path='contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
