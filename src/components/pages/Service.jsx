29a1a8b5d125e5f9797b3d365029338809830217
devin-lynch <devin.l.lynch@gmail.com>
2022-09-30 14:12:19 -0500

    Add info if user routes to a non service

--- src/components/pages/Service.jsx
+++ src/components/pages/Service.jsx
@@ -6,6 +6,10 @@ export default function Service(props) {
     const service = props.services.find((service) => 
     service.id.toString() === id)
     // console.log('ONE SERVICE', service)
+    if(!service) {
+        return <h3>We don't offer that service!</h3>
+    }
+    // !service ? <h3>We don't offer that service!</h3> : null
     return (
         <div>
             <h3>{service.name}</h3>
