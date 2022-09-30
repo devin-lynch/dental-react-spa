342895b374934509aa200ce7e926c5398a16a653
devin-lynch <devin.l.lynch@gmail.com>
2022-09-30 12:55:28 -0500

    Add services prop

--- src/App.js
+++ src/App.js
@@ -6,12 +6,23 @@ import Contact from "./components/pages/Contact"
 import Header from "./components/partials/Header"
 
 export default function App() {
+
+  let services = [
+    "Deep Cleaning",
+    "Filling",
+    "Gum Massage",
+    "Root Canal",
+    "Oral Mud Bath",
+  ]
+
   return (
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='services/*' element={<Services />} />
+          <Route
+            path='services/*'
+            element={<Services services={services} />} />
           <Route path='contact' element={<Contact />} />
         </Routes>
       </BrowserRouter>
