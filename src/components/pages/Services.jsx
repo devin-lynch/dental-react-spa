9f99555b274ffc3a7d45e2389dd73a2184a7a68c
devin-lynch <devin.l.lynch@gmail.com>
2022-09-30 13:00:15 -0500

    Render services array

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,7 +1,13 @@
-export default function Services() {
+export default function Services(props) {
+
+    const servicesArr = props.services.map((service, i) => {
+        return <li key={`service-${i}`}>{service}</li>
+    })
+
     return (
         <div>
             <h1>Services</h1>
+            <ol>{servicesArr}</ol>
         </div>
     )
 }
